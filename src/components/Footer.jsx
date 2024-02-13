6d7f3e3dc4b304a8f44c02971046e14ec5bd1352
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2024-02-13 10:48:57 -0600

    RESTRUCTURED FILES TO ROOT FOLDER - Deleted 'react-challenge-20' folder and have everything in root folder now.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+// Here we are importing a CSS file as a dependency
+import '../styles/Footer.css';
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa6";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className='social-icons'>
+
+        <a href='https://www.linkedin.com/in/tisha-anderson' className='icon' alt="LinkedIn Icon">
+          <FaLinkedin />
+        </a>
+
+        <a href='https://github.com/tishaanderson?tab=repositories' className='icon' alt="Github Icon">
+          <FaGithub />
+        </a>
+
+        <a href='https://www.instagram.com/sweet.tea91/' className='fa icon' alt="Instagram Icon">
+          <FaInstagram />
+        </a>
+
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
