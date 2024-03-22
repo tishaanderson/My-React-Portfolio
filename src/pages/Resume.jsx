ecf2a4a15e7ffa051c0d9875fdfe80779970d945
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2024-03-21 21:22:25 -0500

    Adjusted css on Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -19,74 +19,106 @@ export default function Resume() {
         <p className='job-years'>2020-2023</p>
         <h6 className='job-title'>Quality Assurance Lead <i className='company-name'>Custom Ink</i></h6>
         <ul className='job-responsibilities'>
-          <li className='job-list'>Provide leadership and guidance to the quality assurance team, ensuring
-            alignment with organizational goals and objectives.</li>
-          <li className='job-list'>Assign tasks and delegate responsibilities effectively to ensure the
-            team’s success.</li>
-          <li className='job-list'>Develop and implement standard operating procedures (SOPs) to
-            ensure consistent and efficient processes.</li>
-          <li className='job-list'>Identify areas for process improvement and collaborate with other
-            departments to properly implement solutions.</li>
-          <li className='job-list'>Train and mentor quality assurance team members on quality standards,
+          <ul className='job-list'>Provide leadership and guidance to the quality assurance team, ensuring
+            alignment with organizational goals and objectives.</ul>
+          <ul className='job-list'>Assign tasks and delegate responsibilities effectively to ensure the
+            team’s success.</ul>
+          <ul className='job-list'>Develop and implement standard operating procedures (SOPs) to
+            ensure consistent and efficient processes.</ul>
+          <ul className='job-list'>Identify areas for process improvement and collaborate with other
+            departments to properly implement solutions.</ul>
+          <ul className='job-list'>Train and mentor quality assurance team members on quality standards,
             processes, and protocols.
-          </li>
-          <li className='job-list'>Maintain a customer-centric approach by prioritizing quality and
-            ensuring customer satisfaction.</li>
-          <li className='job-list'>Identify potential risks to quality and develop strategies to mitigate them.</li>
-          <li className='job-list' id='last-job-list'>Proactively address issues that may impact product quality, safety or
+          </ul>
+          <ul className='job-list'>Maintain a customer-centric approach by prioritizing quality and
+            ensuring customer satisfaction.</ul>
+          <ul className='job-list'>Identify potential risks to quality and develop strategies to mitigate them.</ul>
+          <ul className='job-list' id='last-job-list'>Proactively address issues that may impact product quality, safety or
             compliance.
-          </li>
+          </ul>
         </ul>
 
         <p className='job-years'>2016-2020</p>
         <h6 className='job-title'>Bartender <i className='company-name'>Buffalo Wild Wings</i></h6>
         <ul className='job-responsibilities'>
-          <li className='job-list'>Provide exceptional customer service by taking orders accurately and
-            engaging with patrons courteously.</li>
-          <li className='job-list'>Mix and serve a variety of drinks according to standard recipes and
-            customer preferences.</li>
-          <li className='job-list'>Process payments accurately using cash registers or POS systems and
+          <ul className='job-list'>Provide exceptional customer service by taking orders accurately and
+            engaging with patrons courteously.</ul>
+          <ul className='job-list'>Mix and serve a variety of drinks according to standard recipes and
+            customer preferences.</ul>
+          <ul className='job-list'>Process payments accurately using cash registers or POS systems and
             maintain cash drawer integrity.
-          </li>
-          <li className='job-list' id='last-job-list'>Promote specials and upsell premium drinks to enhance sales and
+          </ul>
+          <ul className='job-list' id='last-job-list'>Promote specials and upsell premium drinks to enhance sales and
             customer experience.
-          </li>
+          </ul>
         </ul>
 
         <p className='job-years'>Summer 2016</p>
         <h6 className='job-title'>Front Desk Associate <i className='company-name'>FYZICAL Therapy and Balance Centers</i></h6>
-        <ul className='job-responsibilities'>
-          <li className='job-list'>Communicate effectively via phone, email, and in-person with clients.</li>
-          <li className='job-list'>Excel in multitasking clerical tasks</li>
-          <li className='job-list' id='last-job-list'>Demonstrate excellent organizational skills
-          </li>
+        <ul className='job-responsibilities' id='summer-job'>
+          <ul className='job-list'>Communicate effectively via phone, email, and in-person with clients.</ul>
+          <ul className='job-list'>Excel in multitasking clerical tasks</ul>
+          <ul className='job-list' id='last-job-list'>Demonstrate excellent organizational skills
+          </ul>
         </ul>
       </div>
 
       <div className='resume-card' id='skills-section'>
         <h3 className='resume-section'>Skills</h3>
-        <ul>
-          <li>Full-Stack Development</li>
-          <li>Quality Assurance</li>
-          <li>Applied Behavior Analysis</li>
-          <li>Programming Languages: 
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>SQL</li>
-            <li>NoSQL</li>
-          </li>
-          <li>REACT</li>
-          <li>Node</li>
-          <li>Problem Solving</li>
-          <li>Strategic Decision Making</li>
-          <li>Reinforcement Strategies</li>
-          <li>Organization and Time Management</li>
-          <li>Knowledgeable on creating SOPs, effective streamline processes and cost-savings strategies</li>
-          <li>Strong Analytical Abilities</li>
-          <li>Deleloping Quality Standards</li>
-          <li>Task Prioritization</li>
-        </ul>
+        <div className='skills-container'>
+
+          <ul className='skills'>
+            <h5 className='skill-type'>Fields:</h5>
+
+            <li className='skill-list'>Full-Stack Development</li>
+            <li className='skill-list'>Quality Assurance</li>
+            <li className='skill-list'>Applied Behavior Analysis</li>
+          </ul>
+
+
+
+          <ul className='skills'>
+            <h5 className='skill-type'>Programming Languages:</h5>
+
+            <li className='skill-list'>HTML</li>
+            <li className='skill-list'>CSS</li>
+            <li className='skill-list'>JavaScript</li>
+            <li className='skill-list'>SQL</li>
+            <li className='skill-list'>NoSQL</li>
+
+          </ul>
+
+
+
+          <ul className='skills'>
+            <h5 className='skill-type'>Coding:</h5>
+
+            <li className='skill-list'>REACT</li>
+            <li className='skill-list'>Node</li>
+          </ul>
+        </div>
+
+        <div>
+          <h5 className='skill-type' id='personal-skills'>Personal Strengths:</h5>
+        </div>
+
+        <div className='strengths'>
+          <ul className='skills'>
+            
+
+            <li className='skill-list'>Problem Solving</li>
+            <li className='skill-list'>Strategic Decision Making</li>
+            <li className='skill-list'>Reinforcement Strategies</li>
+            <li className='skill-list'>Organization and Time Management</li>
+          </ul>
+          <ul className='skills' id=''>
+            <li className='skill-list'>Knowledgeable on creating SOPs, effective streamline processes and cost-savings strategies</li>
+            <li className='skill-list'>Strong Analytical Abilities</li>
+            <li className='skill-list'>Deleloping Quality Standards</li>
+            <li className='skill-list'>Task Prioritization</li>
+          </ul>
+        </div>
+
       </div>
     </div>
   );
