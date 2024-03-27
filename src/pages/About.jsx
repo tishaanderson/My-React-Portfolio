15ede9d225622509defe1d36fa6811516c5a32ec
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2024-03-26 23:54:29 -0500

    REVISED ABOUT ME PAGE AND INCREASED FONT SIZE

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,20 +6,20 @@ export default function About() {
       <p className='about-text'>
         Hi there, I'm Tisha Anderson, a recently certified junior software developer embarking on a transformative journey in the tech world. As I step into this dynamic field, I'm driven by the belief that technology holds the key to financial freedom and empowerment in today's rapidly changing world.
       </p>
-      <p className='about-text'> 
-        Born and raised in a small town, I've witnessed firsthand the challenges faced by local businesses and communities. Inspired by the opportunity to make a difference, I'm determined to leverage my newfound skills to help build up small businesses, strengthen community ties, and foster economic growth at the grassroots level.
+      <p className='about-text'>
+        Coming from a small town, I've witnessed firsthand the challenges faced by local businesses and communities. Inspired by the opportunity to make a difference, one of my core career objectives is to simplify technology and enhance convenience for everyday people, thereby empowering small-businesses and communities in an increasingly challenging economic environment.
       </p>
       <p className='about-text'>
         My journey into software development is fueled by more than just professional ambition—it's a testament to the resilience and determination of my family. As the first woman in my family to graduate from college, I'm driven by the desire to create generational wealth and pave the way for future generations to thrive.
       </p>
       <p className='about-text'>
-        Eager and willing to embrace every opportunity for learning and growth, I approach each new challenge with an open mind and a commitment to excellence. My goal is not just to succeed in the tech world but to make a meaningful impact that resonates far beyond the confines of a computer screen.    
+        Eager and willing to embrace every opportunity for learning and growth, I approach each new challenge with an open mind and a commitment to excellence. My goal is not just to succeed in the tech world but to make a meaningful impact that resonates far beyond the confines of a computer screen.
       </p>
       <p className='about-text'>
-        Inspired by my diverse background and a love for simplicity and positive energy, I bring a unique perspective to the tech industry. With a focus on creating relaxed, welcoming environments and infusing creativity and authenticity into my work, I strive to foster a culture of collaboration and innovation. By embracing these values, I aim to not only excel as a software developer but also create spaces where everyone feels empowered to bring their best selves to the table.
+        Inspired by my socioeconomic background and a love for simplicity and positive energy, I believe I can offer a fresh perspective to the tech industry. With a focus on creating relaxed, welcoming environments and infusing creativity and authenticity into my work , I strive to foster an environment of trust, hope, and practicality within local communities. By embracing these values, my goal is not only to excel as a software developer but also create spaces where everyone feels secure and has the opportunity for economic growth.
       </p>
       <p className='about-text'>
-        Join me on this eclectic journey as we explore the intersection of technology and community, innovation and inspiration. Together, let's harness the power of technology to create a brighter, more connected world for all.
+        Join me on this heartfelt journey as we explore the intersection of technology and community, innovation and inspiration. Together, let's harness the power of technology to unite, inspire, and create a more connected and supportive global community.
       </p>
     </div>
   );
